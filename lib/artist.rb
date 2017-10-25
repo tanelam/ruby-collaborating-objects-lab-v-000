7cb97fcb2f72ef1621b077881d587b386d8185b3
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-25 14:26:12 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -23,15 +23,6 @@ class Artist
     @@all << self
   end
 
-  #def self.find_or_create_by_name(name)
-  #    if @@all.any? { |song| song.name == name }
-  #       @@all.find { |song| song.name == name }
-  #   else
-  #      new_name = self.new(name)
-  #      @@all << new_name
-  #      @@all.last
-  #    end
-  #end
   def self.create(name)
     artist = self.new(name)
     artist.name = name
