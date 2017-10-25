9b4a3240816dc8c0995b84e98ab74e1cdfd65718
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-25 13:21:50 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,8 +9,10 @@ class MP3Importer
   end
 
   def import(files)
-    files.each do |file| 
+    files.each do |file|
       file.split(" - ")
+      Artist.find_or_create_by_name(name)
+      Artist.all.size << Song.new_by_filename(filename)
     end
   end
 
