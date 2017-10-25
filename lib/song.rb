7783c106c2a4c180f546b3155d5e642be2d42c27
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-25 12:25:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,11 +6,11 @@ attr_accessor :artist, :name
   end
 
 
-  def self.new_by_filename
-    
-    data = row.split(" - ")
+  def self.new_by_filename(filename)
 
-    song = self.new()
+    artist_name, some_name = filename.split(" - ")
+
+    song = self.new(song)
 
     new_song.artist = Artist.find_or_create_by_name(artist_name)
     song
