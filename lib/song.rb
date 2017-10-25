20a986cef4e605cca4fc107096d147490e037c46
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-25 14:09:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,6 @@ attr_accessor :artist, :name
 
 
   def self.new_by_filename(file)
-
       artist_name = file.split(" - ")[0]
       song_name = file.split(" - ")[1]
       song = self.new(song_name)
