0f694fdbf8c482b10838a5d0db03dd249af39178
Tania Aparicio <tania.e.aparicio@gmail.com>
2017-10-25 13:02:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,10 +7,20 @@ attr_accessor :artist, :name
 
 
   def self.new_by_filename(filename)
-    artist_name, song_name = filename.split(" - ")
-    song = self.new(new_song)
-    new_song.artist = Artist.find_or_create_by_name(artist_name)
-    
+    #artist_name, song_name = filename.split(" - ")
+    #song = self.new(new_song)
+    #new_song.artist = Artist.find_or_create_by_name(artist_name)
+    row = filename_data
+      data = row.split(" - ")
+      artist_name = data[0]
+      song_name = data[1].gsub(".mp3", "")
+
+      song = self.new
+
+      song.name = song_name
+      song.artist_name = artist_name
+
+      song
   end
 
 
